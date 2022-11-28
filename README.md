283c2de5e5dca0f30c5e356eb4f37f68b9dc4083
shahnozahaydarova <shahnozahaydarova03@gmail.com>
2022-11-28 22:21:42 +0500

    new

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### 1. Mathematical function - Factorial.
-### 2. Karta raqamini tekshirish.
+### 2. Plastik karta raqami kiritilganda humo yoki uzcard ekanligini aniqlab beradigan dastur.
+### 3. Ism familiya ochistva  sridingda takrorlash.
--- karta.py
+++ karta.py
@@ -1,19 +1,18 @@
-try:
-    while True:
-        a = input("Karta raqamingizni kiriting: ")
-        if len(a) == 16:
-            if a[0:4] == 8600:
-                print("Uzcard karta raqamini kiritdingiz.Davom etishingiz mumkin.Qaysi xizmat turidan foydalanishni xoxlaysiz.")
-                break
-            elif a[0:4] == '9860':
-                print("Humo karta raqamini kiritdingiz.Davom etishingiz mumkin.Qaysi xizmat turidan foydalanishni xoxlaysiz.")
-                break
-            else:
-                print("Siz kiritgan ma'lumot bo'yicha plastik karta topilmadi qayta urinib ko'ring.")
+while True:
+    a = input("Karta raqamingizni kiriting: ")
+    if len(a) == 16:
+        if a[0:4] == '8600':
+            print("Uzcard karta raqamini kiritdingiz.Davom etishingiz mumkin.Qaysi xizmat turidan foydalanishni xoxlaysiz.")
+            break
+        elif a[0:4] == '9860':
+            print("Humo karta raqamini kiritdingiz.Davom etishingiz mumkin.Qaysi xizmat turidan foydalanishni xoxlaysiz.")
+            break
         else:
-            print("Karta raqami 16ta raqamdan iborat bo'lishi kerak qayta urinib ko'ring.")
-except ValueError:
-    print("Siz karta raqamini kiritish o'rniga harf kiritdingiz,qayta urinib ko'ring.")
+            print("Siz kiritgan ma'lumot bo'yicha plastik karta topilmadi qayta urinib ko'ring.")
+    else:
+        print("Karta raqami 16ta raqamdan iborat bo'lishi kerak qayta urinib ko'ring.")
+
+
 
 # a input deb olingan
 
